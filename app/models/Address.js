e31c560fac299268754e754fc5cccf6361ade0c7
guptakanksha1992 <guptakanksha1992@gmail.com>
2017-09-21 15:15:34 -0400

    Address model

--- app/models/Address.js
+++ app/models/Address.js
@@ -1,35 +1,81 @@
 'use strict';
 
 const _ = require('underscore');
-//const InvalidInputException = require("../exceptions/invalid-input-exception");
-//const Utils = require("../utilities/utils");
+CONTAINS_DIGIT_REGEX = /.*[0-9].*/;
 
 
 var ATTRIBUTES_ADDRESS_REQUIRED = [
   "id",
-  "aptno",
   "streetnumber",
   "street1",
   "street2",
   "city",
   "state",
-  "postalCode",
+  "zipCode",
   "deleted"
 ];
 
+// Check for valid alphabet or not
+static isAlphabeticString(string) {
+  var isNotEmpty = !isEmpty(string);
+  var doesNotContainDigits = !CONTAINS_DIGIT_REGEX.test(string);
+  var doesNotContainInvalidChars = !/[\{\}\(\)_\$#"'@\|!\?\+\*%<>/]/.test(string);
+  return isNotEmpty && doesNotContainDigits && doesNotContainInvalidChars;
+}
+
+static isEmpty(val) {
+    var isUndefined = typeof val === 'undefined';
+    var isNull = val === null;
+    var isString = typeof val === 'string';
+    var isObject = typeof val === 'object';
+
+    if (isUndefined ||  isNull) {
+      return true;
+    } else if (isString) {
+      var hasJustSpaces = val.trim().length === 0;
+      return hasJustSpaces;
+    } else if (isObject) {
+      return isEmptyObject(val);
+    } else {
+      return false;
+    }
+  }
+
+  static isValid(object) {
+    if (typeof object !== 'undefined') {
+      var isValid = true;
+      if (typeof object.validate === 'function') {
+        var validationOutput = object.validate();
+        if (typeof validationOutput === 'boolean') {
+          var isValid = isValid && (validationOutput === true);
+        }
+      }
+      if (typeof object.isValid === 'function') {
+        var validationOutput = object.isValid();
+        if (typeof validationOutput === 'boolean') {
+          var isValid = isValid && (validationOutput === true);
+        }
+      }
+      return isValid;
+    } else {
+      return false;
+    }
+  }
+
+
 const isValidCity = (city) => {
-  return Utils.isAlphabeticString(city);
+  return isAlphabeticString(city);
 }
 const isValidState = (state) => {
-  return Utils.isAlphabeticString(state);
+  return isAlphabeticString(state);
 }
 const isValidId = (id) => {
-  var isNotEmpty = !Utils.isEmpty(id);
+  var isNotEmpty = !isEmpty(id);
   return isNotEmpty;
 }
 const isValidZipCode = (zipCode) => {
   const ZIP_CODE_FORMAT_REGEX = /[0-9]{5,}$/;
-  var isNotEmpty = !Utils.isEmpty(zipCode);
+  var isNotEmpty = !isEmpty(zipCode);
   var hasZipCodeFormat = ZIP_CODE_FORMAT_REGEX.test(zipCode);
   return isNotEmpty && hasZipCodeFormat
 }
@@ -42,11 +88,11 @@ module.exports = class Address {
   constructor(attributes) {
     if (_.isObject(attributes)) {
       this.id = attributes.id;
+      this.streetnumber = attributes.streetnumber;
+      this.street1 = attributes.street1;
+      this.street2 = attributes.street2;
       this.city = attributes.city;
       this.state = attributes.state;
-      this.apt = attributes.apt;
-      this.number = attributes.number;
-      this.street = attributes.street;
       this.zipCode = attributes.zipCode;
       this.deleted = attributes.deleted || false;
     }
@@ -58,7 +104,7 @@ module.exports = class Address {
       if (isValidId(id)) {
         this._id = id;
       } else {
-        throw new InvalidInputException("id")
+        throw new Exception("Invalid id")
       }
     }
   }
@@ -68,7 +114,7 @@ module.exports = class Address {
       if (isValidCity(city)) {
         this._city = city;
       } else {
-        throw new InvalidInputException("city")
+        throw new Exception("Invalid city")
       }
     }
   }
@@ -78,37 +124,37 @@ module.exports = class Address {
       if (isValidState(state)) {
         this._state = state;
       } else {
-        throw new InvalidInputException("state")
+        throw new Exception("Invalid state")
       }
     }
   }
 
-  set apt(apt) {
-    if (apt) {
-      if (!Utils.isEmpty(apt)) {
-        this._apt = apt;
+  set streetnumber(streetnumber) {
+    if (streetnumber) {
+      if (!isEmpty(streetnumber)) {
+        this.streetnumber = streetnumber;
       } else {
-        throw new InvalidInputException("apt")
+        throw new Exception("Invalid street")
       }
     }
   }
 
-  set number(number) {
-    if (number) {
-      if (!Utils.isEmpty(number)) {
-        this._number = number;
+    set street1(street1) {
+    if (street1) {
+      if (!isEmpty(street1)) {
+        this.street1 = street1;
       } else {
-        throw new InvalidInputException("number")
+        throw new Exception("Invalid street1")
       }
     }
   }
 
-  set street(street) {
-    if (street) {
-      if (!Utils.isEmpty(street)) {
-        this._street = street;
+    set street2(street2) {
+    if (street2) {
+      if (!isEmpty(street2)) {
+        this.street2 = street2;
       } else {
-        throw new InvalidInputException("street")
+        throw new Exception("Invalid street2")
       }
     }
   }
@@ -118,7 +164,7 @@ module.exports = class Address {
       if (isValidZipCode(zipCode)) {
         this._zipCode = zipCode;
       } else {
-        throw new InvalidInputException("zip code")
+        throw new Exception("zip code")
       }
     }
   }
@@ -133,10 +179,6 @@ module.exports = class Address {
     return this._id;
   }
 
-  get apt() {
-    return this._apt;
-  }
-
   get city() {
     return this._city;
   }
@@ -145,12 +187,16 @@ module.exports = class Address {
     return this._state;
   }
 
-  get number() {
-    return this._number;
+  get streetnumber() {
+    return this.streetnumber;
   }
 
-  get street() {
-    return this._street;
+    get street1() {
+    return this.street1;
+  }
+
+    get street2() {
+    return this.street2;
   }
 
   get zipCode() {
@@ -164,9 +210,9 @@ module.exports = class Address {
   validate() {
     _.each(VALID_ADDRESS_REQUIRED_ATTRIBUTES, (attribute) => {
       var hasAttribute = typeof this[attribute] !== 'undefined';
-      var isValidAttribute = Utils.isValid(this[attribute]);
+      var isValidAttribute = isValid(this[attribute]);
       if (!hasAttribute || !isValidAttribute) {
-        throw new InvalidInputException(attribute);
+        throw new Exception(attribute);
       }
     });
     return true;
