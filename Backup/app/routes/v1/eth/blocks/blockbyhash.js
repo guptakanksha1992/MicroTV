6e27deb9cae60a8346c876dfcc2fdc0517e7d932
AbhishekShah212 <abhishah212@gmail.com>
2017-09-21 21:37:11 -0500

    invalidating repo

--- Backup/app/routes/v1/eth/blocks/blockbyhash.js
+++
@@ -1,4 +0,0 @@
-module.exports = function(req, res) {
-    req.app.locals.log.info("/v1/eth/blocks/blockbyhash function"); 
-    res.json(req['block']); 
-}; 
--- app/services/person/scripts/productionRun.sh
+++
@@ -1,2 +0,0 @@
-# start server
-node server.js
